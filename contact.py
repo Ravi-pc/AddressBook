b1373fe359bf1ad90e3724c7e5a68d9e372fbcdf
“Ravi-pc” <ravisingh98138@gmail.com>
2023-12-14 14:18:31 +0530

    [Rav] Add: Proggram to add multiple address book in the dictionary

--- contact.py
+++ contact.py
@@ -21,6 +21,52 @@ class Contact:
         self.ph_no = ph_no
         self.e_mail = e_mail
 
+    def update_contact(self):
+        """
+            Description: update_contact function is used to update the details of a person
+                         from the address book.
+
+            Parameter: First Name
+
+            Return:    None
+
+        """
+        while True:
+            print(f'1. Update First Name.\n'
+                  f'2. Update Last Name.\n'
+                  f'3. Update Address\n'
+                  f'4. Update City\n'
+                  f'5. Update Zip Code\n'
+                  f'6. Update Phone Number\n'
+                  f'7. Update E-Mail\n'
+                  f'0. Exit'
+                  )
+            option = int(input('Enter the option:  '))
+            match option:
+                case 1:
+                    change = input(f'Enter new first name: ')
+                    self.f_name = change
+                case 2:
+                    change = input(f'Enter new last name: ')
+                    self.l_name = change
+                case 3:
+                    change = input(f'Enter new Address: ')
+                    self.address = change
+                case 4:
+                    change = input(f'Enter new City: ')
+                    self.city = change
+                case 5:
+                    change = input(f'Enter new Zip Code: ')
+                    self.zip_code = change
+                case 6:
+                    change = input(f'Enter new Phone Number: ')
+                    self.ph_no = change
+                case 7:
+                    change = input(f'Enter new E-Mail: ')
+                    self.e_mail = change
+                case 0:
+                    break
+
 
 class AddressBook:
     def __init__(self, book_name):
@@ -39,55 +85,12 @@ class AddressBook:
         """
         self.contact_dict.update({contact_obj.f_name: contact_obj})
 
-    def update_contact(self, f_name):
-        """
-            Description: update_contact function is used to update the details of a person
-                         from the address book.
-
-            Parameter: First Name
-
-            Return:    None
-
-        """
-        contact_obj: Contact = self.contact_dict.get(f_name)
+    def modify_contact(self, name):
+        contact_obj: Contact = self.contact_dict.get(name)
         if contact_obj:
-            while True:
-                print(f'1. Update First Name.\n'
-                      f'2. Update Last Name.\n'
-                      f'3. Update Address\n'
-                      f'4. Update City\n'
-                      f'5. Update Zip Code\n'
-                      f'6. Update Phone Number\n'
-                      f'7. Update E-Mail\n'
-                      f'0. Exit'
-                      )
-                option = int(input('Enter the option:  '))
-                match option:
-                    case 1:
-                        change = input(f'Enter new first name: ')
-                        self.contact_dict.update({f_name: change})
-                    case 2:
-                        change = input(f'Enter new last name: ')
-                        self.contact_dict.update({f_name: change})
-                    case 3:
-                        change = input(f'Enter new Address: ')
-                        self.contact_dict.update({f_name: change})
-                    case 4:
-                        change = input(f'Enter new City: ')
-                        self.contact_dict.update({f_name: change})
-                    case 5:
-                        change = input(f'Enter new Zip Code: ')
-                        self.contact_dict.update({f_name: change})
-                    case 6:
-                        change = input(f'Enter new Phone Number: ')
-                        self.contact_dict.update({f_name: change})
-                    case 7:
-                        change = input(f'Enter new E-Mail: ')
-                        self.contact_dict.update({f_name: change})
-                    case 0:
-                        break
+            contact_obj.update_contact()
         else:
-            print(f'No Element found of this name')
+            print("Element NOt Found or Dictionary is empty. ")
 
     def delete_contact(self, first_name):
         """
@@ -105,6 +108,38 @@ class AddressBook:
         else:
             print(f'Name not found or dictionary is empty.')
 
+    def display_contact(self):
+        """
+                Description: display_contact function is to display the details of a contact
+                             from the address book.
+
+                Parameter: None
+
+                Return:    None
+
+        """
+        for key, value in self.contact_dict.items():
+            print(f"""
+                     First Name: {key} 
+                     Last Name: {value.l_name} 
+                     Address : {value.address}
+                     City : {value.city}
+                     Zip Code : {value.zip_code}
+                     Phone Number: {value.ph_no}
+                     E-Mail : {value.e_mail}  
+             """)
+
+
+class MultipleAddressBook:
+    def __init__(self):
+        self.address_book_dict = {}
+
+    def add_address_book(self, address_book_obj):
+        self.address_book_dict.update({address_book_obj.book_name: address_book_obj})
+
+    def get_address_book(self, book_name):
+        return self.address_book_dict.get(book_name)
+
 
 def contact_details():
     """
@@ -116,18 +151,23 @@ def contact_details():
         Return:    None
 
     """
-    book_name = input('Enter the name of the Address Book: ')
-    address_book_obj = AddressBook(book_name)
+    multi_address_book_obj = MultipleAddressBook()
     while True:
-        print(f'1. Add New Contact\n'
-              f'2. Update an Existing Contact\n'
-              f'3. Delete an Existing Contact\n'
+        print(f'1. Add a new Address Book\n'
+              f'2. Add New Contact\n'
+              f'3. Update an Existing Contact\n'
+              f'4. Delete an Existing Contact\n'
+              f'5. Display the Contacts\n'
               f'0. Exit ')
         choice = int(input('Enter your choice: '))
         match choice:
             case 0:
                 break
             case 1:
+                book_name = input('Enter the name of the Address Book: ')
+                address_book_obj = multi_address_book_obj.get_address_book(book_name)
+                if address_book_obj is None:
+                    address_book_obj = AddressBook(book_name)
                 f_name = input('Enter the First name of the Person ')
                 l_name = input('Enter the Last name of the Person ')
                 address = input('Enter the Address of the Person ')
@@ -135,14 +175,24 @@ def contact_details():
                 zip_code = input('Enter the Zip code of the area ')
                 ph_no = input('Enter the Phone Number of the Person ')
                 email = input('Enter the E-Mail of the Person ')
-                contact_obj = Contact(f_name, l_name, address, city, zip_code, ph_no, email, )
+                contact_obj = Contact(f_name, l_name, address, city, zip_code, ph_no, email)
                 address_book_obj.add_contact(contact_obj)
+                multi_address_book_obj.add_address_book(address_book_obj)
+
             case 2:
+                book_name = input('Enter the name of the Address Book: ')
+                address_book_obj = multi_address_book_obj.get_address_book(book_name)
                 name = input('Enter the first name of the contact: ')
-                address_book_obj.update_contact(name)
+                address_book_obj.modify_contact(name)
             case 3:
+                book_name = input('Enter the name of the Address Book: ')
+                address_book_obj = multi_address_book_obj.get_address_book(book_name)
                 first_name = input('Enter the name of the contact you want to delete: ')
                 address_book_obj.delete_contact(first_name)
+            case 4:
+                book_name = input('Enter the name of the Address Book: ')
+                address_book_obj = multi_address_book_obj.get_address_book(book_name)
+                address_book_obj.display_contact()
 
 
 if __name__ == "__main__":
